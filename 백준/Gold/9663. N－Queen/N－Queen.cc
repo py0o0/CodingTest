2241949534bfa236f93d4a80d799ac4276987960
py0o0 <pantom0425@naver.com>
2024-06-06 16:47:16 +0900

    [Gold IV] Title: N-Queen, Time: 4036 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int map[13];
+
+int cnt;
+
+bool possible(int x,int y){
+    for(int i=0;i<y;i++){
+        if(map[i] == x || abs(map[i] - x) == y - i)
+            return false;
+    }
+    return true;
+}
+
+void dfs(int n,int a){
+    if(a == n){
+        cnt++;
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(possible(i,a)){
+            map[a] = i;
+            dfs(n,a+1);
+            map[a] = 0;
+        }
+    }
+    
+}
+
+int solution(int n) {
+    int answer = 0;
+    
+    dfs(n,0);
+    
+    answer = cnt;
+    return answer;
+}
+
+int main(void){
+    int n;
+    cin>>n;
+    cout<<solution(n);
+}
