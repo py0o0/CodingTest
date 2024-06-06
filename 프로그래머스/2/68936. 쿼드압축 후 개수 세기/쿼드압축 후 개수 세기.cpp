e93109c6eacdc2ce8383aa485017c9c9bbff7e46
py0o0 <pantom0425@naver.com>
2024-06-06 15:17:55 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 2.60 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -3,7 +3,52 @@
 
 using namespace std;
 
+int zero;
+int one;
+
+int map[1025][1025];
+
+void dfs(int x,int y, int size){
+    int a = map[x][y];
+    
+    int fail = 0;
+    for(int i=x;i<x + size;i++){
+        for(int j=y;j<y + size;j++){
+            if(map[i][j] != a){
+                fail = 1;
+                break;
+            }
+        }
+        if(fail)
+            break;
+    }
+    
+    if(!fail){
+        if(a == 1)
+            one++;
+        else
+            zero++;
+        return;
+    }
+    
+    dfs(x,y,size/2);
+    dfs(x + size/2,y,size/2);
+    dfs(x,y+ size/2,size/2);
+    dfs(x+ size/2,y+ size/2,size/2);
+    
+    
+}
+
 vector<int> solution(vector<vector<int>> arr) {
     vector<int> answer;
+    
+    for(int i=0;i<arr.size();i++)
+        for(int j=0;j<arr[0].size();j++)
+            map[i][j] = arr[i][j];
+    
+    dfs(0,0,arr.size());
+    
+    answer.push_back(zero);
+    answer.push_back(one);
     return answer;
 }
