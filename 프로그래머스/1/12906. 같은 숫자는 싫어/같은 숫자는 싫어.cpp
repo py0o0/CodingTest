5fb3baff3fa7951a25f268b83610fb603721c511
py0o0 <pantom0425@naver.com>
2024-06-06 13:22:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    for(int i=0;i<arr.size();i++){
+        if(answer[answer.size()-1]!=arr[i])
+            answer.push_back(arr[i]);
+    }
+        
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
