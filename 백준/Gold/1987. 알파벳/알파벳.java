af846eb9d405dceda5ca843ead7b61c7d89aaf01
py0o0 <pantom0425@naver.com>
2025-01-26 17:18:22 +0900

    [Gold IV] Title: 알파벳, Time: 768 ms, Memory: 16392 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,43 +1,49 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+    class Main {
 
+        static char[][] map;
+        static int r, c;
+        static int max;
+        static int[] check = new int[26];
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
 
-    static Scanner sc = new Scanner(System.in);
-    static char[][] map;
-    static int[] al;
-    static int Max = 0;
-    public static void main(String[] args){
-        int n,m;
-        n = sc.nextInt();
-        m = sc.nextInt();
-        map = new char[n][m];
-        al = new int[26];
-        for(int i = 0; i < n; i++){
-            String input = sc.next();
-            for(int j = 0; j < m; j++)
-                map[i][j] = input.charAt(j);
+            st = new StringTokenizer(br.readLine());
+
+            r = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            map = new char[r][c];
+
+            for(int i =0; i<r;i++){
+                String line = br.readLine();
+                for(int j =0; j<c;j++)
+                    map[i][j] = line.charAt(j);
+            }
+
+            dfs(0,0,1);
+            System.out.println(max);
+
+        }
+        static void dfs(int x, int y, int cnt){
+            if(x < 0 || y < 0 || x >= r || y >= c) return;
+
+            char c = map[x][y];
+            if(check[c - 'A'] == 1) return;
+
+            check[c - 'A'] = 1;
+            max = Math.max(max, cnt);
+            char c2 = map[x][y];
+            dfs(x, y+1, cnt+1);
+            dfs(x, y-1, cnt+1);
+            dfs(x+1, y, cnt+1);
+            dfs(x-1, y, cnt+1);
+            check[c - 'A'] = 0;
         }
 
-        dfs(0,0,n,m,0);
-        System.out.println(Max);
-    }
-    public static void dfs(int x,int y,int n,int m,int cnt){
-        if(x<0 || y<0 || x>=n || y>=m) return;
-        if(al[map[x][y] - 'A'] > 0) return;
-        cnt++;
-        Max = Math.max(Max,cnt);
-        al[map[x][y] - 'A'] = 1;
-        dfs(x+1,y,n,m,cnt);
-        dfs(x,y+1,n,m,cnt);
-        dfs(x-1,y,n,m,cnt);
-        dfs(x,y-1,n,m,cnt);
-        al[map[x][y] - 'A'] = 0;
 
 
     }
-
-}
