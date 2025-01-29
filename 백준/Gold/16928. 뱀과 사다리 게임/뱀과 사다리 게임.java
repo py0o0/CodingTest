d5713b4aa4f1e22f010e469a57fcaca4efc7209d
py0o0 <pantom0425@naver.com>
2025-01-29 16:30:24 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,78 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+    class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        static int[]move;
+        static int[]visit;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
 
-        int[] map = new int[101];
-        int[] visit = new int[101];
-
-        for(int i = 0; i<101; i++)
-            map[i] = i;
-
-
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        for(int i = 0; i< n+m; i++){
+            int n,k;
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
 
-            map[s] = e;
-        }
+            move = new int[101];
+            visit = new int[101];
+            for (int i = 0; i < n + k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                move[s] = e;
+            }
 
-        Queue<xy> q = new LinkedList<>();
+            Queue<xy> q = new LinkedList<>();
+            for(int i = 1; i<7;i++)
+                q.add(new xy(1,1,i));
+            visit[1] = 1;
 
-        q.add(new xy(1,1));
+            while(!q.isEmpty()) {
+                xy x = q.poll();
+                int next = x.cur + x.move;
 
-        int flag = 0;
-        while(!q.isEmpty()){
-            xy x = q.poll();
-
-            for(int i= 1; i<7; i++){
-                int nx = x.x + i;
-
-                if(nx > 100)
-                    continue;
-
-                nx = map[nx];
-                
-                if(visit[nx] == 1)
-                    continue;
-                
-                visit[nx] = 1;
-                
-                if(nx == 100){
+                if(next == 100){
                     System.out.println(x.cnt);
-                    flag = 1;
-                    break;
+                    return;
                 }
 
-                q.add(new xy(nx,x.cnt + 1));
+                if(next > 100) continue;
+                if(move[next] != 0)
+                    next = move[next];
+                if(visit[next] == 1) continue;
 
+                visit[next] = 1;
+                for(int i = 1; i<7;i++)
+                    q.add(new xy(next,x.cnt + 1,i));
             }
-            if(flag == 1)
-                break;
+
+
+        }
+        static class xy{
+            int cur;
+            int cnt;
+            int move;
+            xy(int cur,int cnt,int move){
+                this.cur = cur;
+                this.cnt = cnt;
+                this.move = move;
+            }
         }
 
 
 
 
     }
-    static public class xy{
-        int x;
-        int cnt;
-        xy(int x, int cnt){
-            this.x = x;
-            this.cnt = cnt;
-        }
-    }
-
-}
