f3db4d7d765d5a94ddc59579d5b550040ffd9256
py0o0 <pantom0425@naver.com>
2025-01-30 18:48:06 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,40 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+    class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
 
-       st = new StringTokenizer(br.readLine());
-       int n = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(br.readLine());
+            int[] v = new int[n];
+            int[][]dp = new int[n][3];
+            for (int i = 0; i < n; i++)
+                v[i] = Integer.parseInt(br.readLine());
+            if(n == 1){
+                System.out.println(v[0]);
+                return;
+            }
+            dp[0][1] = v[0];
+            dp[1][2] = dp[0][1] + v[1];
+            dp[1][1] = v[1];
 
-       int[][] stair = new int[n][3];
-
-       for(int i = 0; i < n; i++){
-           st = new StringTokenizer(br.readLine());
-           stair[i][0] = Integer.parseInt(st.nextToken());
-       }
-       if(n==1){
-           System.out.println(stair[0][0]);
-           return;
-       }
-       
-       stair[0][1] = stair[0][0];
-
-       stair[1][1] = stair[1][0];
-       stair[1][2] = stair[0][1] + stair[1][0];
-       for(int i = 2; i < n; i++){
-           stair[i][1] = Math.max(stair[i-2][1] ,stair[i-2][2]) + stair[i][0];
-           stair[i][2] = stair[i-1][1] + stair[i][0];
-       }
-
-       System.out.println(Math.max(stair[n-1][1], stair[n-1][2]));
-
+            for(int i = 2; i<n;i++){
+                dp[i][2] = dp[i-1][1] + v[i];
+                dp[i][1] = Math.max(dp[i-2][1], dp[i-2][2]) + v[i];
+            }
+            System.out.println(Math.max(dp[n-1][2], dp[n-1][1]));
+        }
     }
-
-
-}
