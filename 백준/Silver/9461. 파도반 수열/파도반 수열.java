32db3686e64b5b9e2aebf6b6b7c782b694296839
py0o0 <pantom0425@naver.com>
2025-01-30 17:53:26 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,29 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+    class Main {
 
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+            long[] dp = new long[101];
+            dp[1] = 1;
+            dp[2] = 1;
+            dp[3] = 1;
+            dp[4] = 2;
+            for(int i = 5; i<101; i++) {
+                dp[i] = dp[i - 3] + dp[i - 2];
+            }
 
-        int t = Integer.parseInt(br.readLine());
-
-        long[]dp = new long[101];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for(int i=3; i<=100;i++)
-            dp[i] = dp[i-2] + dp[i-3];
-
-
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(dp[n]);
+            int t = Integer.parseInt(br.readLine());
+            while(t-- > 0) {
+                int n = Integer.parseInt(br.readLine());
+                System.out.println(dp[n]);
+            }
         }
     }
-
-}
