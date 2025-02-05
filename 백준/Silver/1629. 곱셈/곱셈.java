411ab15e0b3b00f711ac87d1493bad6c08707bbe
py0o0 <pantom0425@naver.com>
2025-02-05 13:28:26 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -19,18 +19,11 @@ class Main{
     static long dfs(long a, long b, long c){
         if(b == 1)
             return a % c;
-
-        long val = dfs(a,b/2,c);
-        val %= c;
-        val = (val * val) % c;
-
-        if(b%2 == 0)
-            return val;
-        else
-            return (a * val) % c;
+        
+        if(b % 2 == 0){
+            return dfs((a * a) % c,b/2,c) % c;
+        }
+        return (a * dfs(a,b-1,c)) % c;
     }
-
-
-
 
 }
