12096d7f6a9b1daeca8d1416573e3aba14c03482
py0o0 <pantom0425@naver.com>
2025-03-12 19:47:14 +0900

    [Gold IV] Title: 좋다, Time: 164 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,48 +1,45 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main{
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args){
-        int n = sc.nextInt();
-        long [] v = new long[n];
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long[] v = new long[n];
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++)
-            v[i] = sc.nextLong();
+            v[i] = Long.parseLong(st.nextToken());
+
         Arrays.sort(v);
+
         int cnt = 0;
-
         for(int i = 0; i < n; i++){
+
             int start = 0;
-            int end = n-1;
-
-            if(i == start)
-                start++;
-            if(i == end)
-                end--;
-
+            if(start == i) start++;
+            
+            int end = n - 1;
+            if(end == i) end--;
             while(start < end){
-                long sum = v[start] + v[end];
-                if(sum == v[i]){
-                    cnt++;
-                    break;
+                if(v[i] == v[start] + v[end]){
+                    cnt++; break;
                 }
-                if(sum < v[i]) {
-                    start++;
-                    if(start == i)
-                        start++;
+                else if(v[i] > v[start] + v[end]) {
+                    start++; if(start ==i) start++;
                 }
-                else if(sum > v[i]) {
-                    end--;
-                    if(end == i)
-                        end--;
+                else if(v[i] < v[start] + v[end]){
+                    end--; if(end == i) end--;
                 }
             }
         }
         System.out.println(cnt);
 
-    }
 
+
+    }
 
 }
