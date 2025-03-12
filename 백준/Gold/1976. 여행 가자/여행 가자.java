6585a344a22ab4b756edd84b9b5beb0555960280
py0o0 <pantom0425@naver.com>
2025-03-12 19:35:14 +0900

    [Gold IV] Title: 여행 가자, Time: 168 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,88 +1,78 @@
-        import javax.lang.model.type.ArrayType;
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.*;
+import java.io.*;
+import java.util.*;
 
-        public class Main {
+class Main{
 
-            static ArrayList<Road>[] roads;
-            static int[][] dp;
-            public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                StringTokenizer st;
-                int n = Integer.parseInt(br.readLine());
-                int m = Integer.parseInt(br.readLine());
+    static ArrayList<Road>[] roads;
+    static int[] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-                roads = new ArrayList[n + 1];
-                dp = new int[n + 1][n + 1];
-                for (int i = 0; i <= n; i++)
-                    roads[i] = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
 
-                for(int i = 1; i <= n; i++){
-                    st = new StringTokenizer(br.readLine());
-                    for(int j = 1; j <= n; j++){
-                        int next = Integer.parseInt(st.nextToken());
-                        if(next == 0) continue;
-                        roads[i].add(new Road(j,next));
-                    }
-                }
-                for(int i = 1; i <= n; i++)
-                    for(int j = 1; j <= n; j++)
-                        dp[i][j] = Integer.MAX_VALUE;
-
-
-                for(int i = 1; i <= n; i++)
-                    dijkstra(i);
-
-                st = new StringTokenizer(br.readLine());
-                int now = Integer.parseInt(st.nextToken());
-                while(m-- > 1){
-                    int next = Integer.parseInt(st.nextToken());
-                    if(dp[now][next] == Integer.MAX_VALUE){
-                        System.out.println("NO"); return;
-                    }
-                    now = next;
-                }
-                System.out.println("YES");
-
-            }
-            static void dijkstra(int start){
-                dp[start][start] = 0;
-                PriorityQueue<xy> pq = new PriorityQueue<>((a,b) -> a.now_t - b.now_t);
-                pq.add(new xy(start,0));
-
-                while(!pq.isEmpty()){
-                    xy x = pq.poll();
-                    for(Road road : roads[x.now]){
-                        if(dp[start][road.next] > dp[start][x.now] + road.t){
-                            dp[start][road.next] = dp[start][x.now] + road.t;
-                            pq.add(new xy(road.next, dp[start][road.next]));
-                        }
-                    }
-                }
-
-            }
-
-            static class xy{
-                int now;
-                int now_t;
-                xy(int now, int now_t){
-                    this.now = now;
-                    this.now_t = now_t;
-                }
-            }
-
-            static class Road{
-                int next;
-                int t;
-                Road(int next, int t){
-                    this.next = next;
-                    this.t = t;
-                }
-            }
-
-
+        roads = new ArrayList[n + 1];
+        dp = new int[n + 1];
+        for(int i = 0; i <= n; i++) {
+            roads[i] = new ArrayList<>();
+            dp[i] = Integer.MAX_VALUE;
         }
 
+        for(int i = 1; i <= n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n;j++){
+                int x = Integer.parseInt(st.nextToken());
 
+                if(x == 1){
+                    roads[i].add(new Road(j,1));
+                    roads[j].add(new Road(i,1));
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        m--;
+        dijstra(x);
+
+        while(m-->0){
+            int a = Integer.parseInt(st.nextToken());
+            if(dp[a] == Integer.MAX_VALUE){
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+
+    }
+    public static void dijstra(int i){
+        dp[i] = 0;
+        PriorityQueue<Road> pq = new PriorityQueue<>((a,b)->a.val-b.val);
+        pq.add(new Road(i,0));
+
+        while(!pq.isEmpty()){
+            Road x = pq.poll();
+            for(Road road : roads[x.x]){
+                if(dp[road.x] > x.val + road.val){
+                    dp[road.x] = x.val + road.val;
+                    pq.add(new Road(road.x,x.val + road.val));
+                }
+            }
+        }
+    }
+    static class Road{
+        int x;
+        int val;
+        Road(int x, int val){
+            this.x = x;
+            this.val = val;
+        }
+    }
+
+
+
+
+}
