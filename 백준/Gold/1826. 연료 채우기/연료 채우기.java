18fc563edc7ae1e86f4d7f88d9a57a899804dbbf
py0o0 <pantom0425@naver.com>
2025-03-18 13:55:20 +0900

    [Gold II] Title: 연료 채우기, Time: 224 ms, Memory: 19916 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -3,66 +3,57 @@ import java.util.*;
 
 class Main{
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()) + 1;
-
-
-        xy[] v = new xy[n];
-
-        for(int i=0; i<n - 1; i++){
+        int n = Integer.parseInt(st.nextToken());
+        xy[] v = new xy[n + 1];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int g = Integer.parseInt(st.nextToken());
             v[i] = new xy();
-            v[i].x = x;
-            v[i].g = g;
+            v[i].x = Integer.parseInt(st.nextToken());
+            v[i].y = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         int target = Integer.parseInt(st.nextToken());
-        int g = Integer.parseInt(st.nextToken());
+        int total = Integer.parseInt(st.nextToken());
+        int cnt = 0;
 
-        v[n - 1] = new xy();
-        v[n - 1].x = target;
-        v[n - 1].g = 0;
+        v[n] = new xy();
+        v[n].x = target;
+        v[n].y = 0;
+        
+        Arrays.sort(v,(a,b)->a.x-b.x);
 
-        Arrays.sort(v,(a,b) -> a.x - b.x);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        int dist = 0;
+        for(int i =0; i<=n; i++){
+            int use = v[i].x - dist;
+            dist = v[i].x;
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b- a);
-
-        int pre = 0;
-        int an = 0;
-        for(int i=0; i<n; i++){
-            int use = v[i].x - pre;
-            g -= use;
-
-            if(g < 0){
-                int flag = 0;
-                while(pq.size() > 0){
-                    g += pq.poll();
-                    an++;
-                    if(g >= 0){
-                        flag = 1; break;
-                    }
+            total -= use;
+            if(total < 0){
+                while(!pq.isEmpty()){
+                    total += pq.poll();
+                    cnt++;
+                    if(total >= 0)
+                        break;
                 }
-                if(flag == 0){
-                    System.out.println(-1);
+                if(total < 0 && pq.isEmpty()){
+                    System.out.println(-1 );
                     return;
                 }
             }
-            pre = v[i].x;
-            pq.add(v[i].g);
+            pq.add(v[i].y);
 
         }
-        System.out.println(an);
+        System.out.println(cnt);
+    }
+    static class xy{
+        int x,y;
     }
 
-    static class xy{
-        int x;
-        int g;
-    }
 }
