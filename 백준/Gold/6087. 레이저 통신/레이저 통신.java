ff633c5c446c59202e29df4a46a1cc5572cbef0e
py0o0 <pantom0425@naver.com>
2025-03-20 13:10:30 +0900

    [Gold III] Title: 레이저 통신, Time: 140 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 class Main{
 
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -13,77 +13,73 @@ class Main{
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
+        int[][] p = new int[2][2];
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 2; j++)
+                p[i][j] = -1;
+
         char[][] map = new char[n][m];
         int[][][] visit = new int[n][m][4];
-
-        xy start = new xy(-1,-1);
-
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = line.charAt(j);
-                if(map[i][j] == 'C') {
-                    if (start.x == -1) {
-                        start.x = i;
-                        start.y = j;
-                        map[i][j] = '.';
-                    }
-                }
                 for(int k = 0; k < 4; k++)
                     visit[i][j][k] = Integer.MAX_VALUE;
+
+                map[i][j] = line.charAt(j);
+                if(map[i][j] == 'C'){
+                    if(p[0][0] == -1){
+                        p[0][0] = i;
+                        p[0][1] = j;
+                        map[i][j] = '.';
+                    }
+                    else{
+                        p[1][0] = i;
+                        p[1][1] = j;
+                    }
+                }
             }
         }
 
-        PriorityQueue<Q> q = new PriorityQueue<>((a,b) -> a.cnt - b.cnt);
-        q.add(new Q(start.x, start.y, 0,-1));
+        PriorityQueue<xy> pq = new PriorityQueue<>((a,b)->a.curb-b.curb);
+        pq.add(new xy(p[0][0],p[0][1],0,-1));
 
-
-        while(!q.isEmpty()){
-            Q x = q.poll();
+        while(!pq.isEmpty()){
+            xy x = pq.poll();
 
             if(map[x.x][x.y] == 'C'){
-                System.out.println(x.cnt);
+                System.out.println(x.curb - 1);
                 return;
             }
 
             for(int i = 0; i < 4; i++){
-                if(x.dir != -1 && Math.abs(x.dir - i) == 2) continue;
+                if(x.dir != -1 && x.dir == (i+2)%4 ) continue;
                 int nx = x.x + dx[i];
                 int ny = x.y + dy[i];
 
-                int val = x.cnt;
-                if(x.dir != i && x.dir != -1) val++;
-
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if(map[nx][ny]== '*') continue;
 
+                int curb = x.curb;
+                if(x.dir != i) curb++;
 
-                if(map[nx][ny] == '*') continue;
-                if(visit[nx][ny][i] <= val) continue;
+                if(visit[nx][ny][i] <= curb) continue;
+                visit[nx][ny][i] = curb;
 
-                visit[nx][ny][i] = val;
-                q.add(new Q(nx,ny,val,i));
+                pq.add(new xy(nx,ny,curb,i));
+
             }
-
         }
 
     }
-    static class Q{
-        int x,y,cnt,dir;
-        Q(int x,int y,int cnt,int dir){
+    static class xy{
+        int x,y,curb,dir;
+        xy(int x,int y,int curb,int dir){
             this.x = x;
             this.y = y;
-            this.cnt = cnt;
+            this.curb = curb;
             this.dir = dir;
         }
     }
-
-    static class xy{
-        int x,y;
-        xy(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
 
 }
