edf22264b5f9e46d1b558bb7cfcbe5fd176a03f9
py0o0 <pantom0425@naver.com>
2025-03-21 14:43:35 +0900

    [Gold V] Title: 흙길 보수하기, Time: 212 ms, Memory: 19812 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -9,51 +9,37 @@ class Main{
 
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        long l = Long.parseLong(st.nextToken());
+        long l = Integer.parseInt(st.nextToken());
 
         xy[] v = new xy[n];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            long s = Long.parseLong(st.nextToken());
-            long e = Long.parseLong(st.nextToken());
             v[i] = new xy();
-            v[i].s = s;
-            v[i].e = e;
+            v[i].s = Long.parseLong(st.nextToken());
+            v[i].e = Long.parseLong(st.nextToken());
         }
 
-        Arrays.sort(v,(a,b)->{
-            return Long.compare(a.s,b.s);
-        });
+        Arrays.sort(v,(a,b)->Long.compare(a.s,b.s));
 
+        long x = v[0].s;
         long an = 0;
-        long pre = 0;
         for(int i = 0; i < n; i++){
-            if(pre >= v[i].e) continue;
-            else if(pre > v[i].s) v[i].s = pre;
+            if(x >= v[i].e) continue;
+            
+            x = Math.max(v[i].s, x);
 
-            long x = v[i].e - v[i].s;
+            long temp = v[i].e - x;
+            long cnt =  temp / l;
 
-            if(l >= x){
-                an++;
-                pre = v[i].s + l;
-            }
+            if(temp % l != 0) cnt++;
 
-            else if(x%l == 0){
-                an += x/l;
-                pre = v[i].e;
-            }
-            else{
-                an += x/l + 1;
-                long val = l - (x%l);
-                pre = v[i].e + val;
-            }
+            x = x + l * cnt;
 
+            an += cnt;
         }
         System.out.println(an);
-
     }
     static class xy{
-        long s;
-        long e;
+        long s,e;
     }
 }
