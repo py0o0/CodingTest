7d4246578efd725a6261f9bf87aa08222daa20f5
py0o0 <pantom0425@naver.com>
2025-05-26 14:35:59 +0900

    [Gold IV] Title: 공유기 설치, Time: 284 ms, Memory: 29768 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,43 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-    class Main {
+public class Main {
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-            long[] v = new long[n];
-            for(int i = 0; i < n; i++)
-                v[i] = Long.parseLong(br.readLine());
+        long[]v = new long[n];
 
-            Arrays.sort(v);
+        for(int i = 0; i < n; i++)
+            v[i] = Long.parseLong(br.readLine());
 
-            long start = 1;
-            long end = v[n - 1];
+        Arrays.sort(v);
 
-            while(start < end){
-                long mid = (start + end) / 2;
+        long start = 1;
+        long end = v[n-1];
 
-                long x = v[0];              //첫 공유기 설치
-                int cnt = 1;
-                for(int i = 1; i<n; i++){
-                    if(x + mid < v[i]){
-                        cnt++;
-                        x = v[i];   //v[i]가 거리 밖에 있을 시 공유기 추가
-                    }
+        while(start < end){
+            long mid = (start + end)/2;
+
+            long x = v[0];
+            int cnt = 1;
+            for(int i = 1; i < n; i++){
+                if(x + mid < v[i]){
+                    cnt++;
+                    x = v[i];
                 }
-
-                if(cnt < m)
-                    end = mid;
-                else
-                    start = mid + 1;
             }
-            System.out.println(start);
+
+            if(cnt < k)
+                end = mid;
+            else
+                start = mid + 1;
         }
+        System.out.println(start);
 
     }
+    
+}
