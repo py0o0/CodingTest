4ea6a758b8cc14645b28a7f6e14e4c355c989923
py0o0 <pantom0425@naver.com>
2025-05-30 17:13:14 +0900

    [Gold IV] Title: 타임머신, Time: 240 ms, Memory: 19560 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,11 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Main{
+public class Main {
 
-    static long[] dp;
     static Road[] roads;
-    public static void main(String[] args) throws IOException{
+    static long[] dp;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -13,56 +15,46 @@ class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        dp = new long[n+1];
+        dp = new long[n + 1];
         roads = new Road[m];
 
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-
             roads[i] = new Road();
-            roads[i].s = s;
-            roads[i].e = e;
-            roads[i].t = t;
+            roads[i].s = Integer.parseInt(st.nextToken());
+            roads[i].e = Integer.parseInt(st.nextToken());
+            roads[i].t = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1; i <= n; i++)
-            dp[i] = Long.MAX_VALUE;
+        for(int i = 1 ; i <= n ; i++) dp[i] = Long.MAX_VALUE;
+        dp[1] = 0;
 
         bellman(1,n);
 
-    }
-    static void bellman(int x, int n){
-        dp[x] = 0;
 
-        for(int i = 1; i <=n; i++){
-            for(Road r : roads){
-                if(dp[r.s] == Long.MAX_VALUE) continue;
+    }static void bellman(int start, int n){
+        dp[start] = 0;
 
-                if(dp[r.e] > dp[r.s] + r.t){
-                    if(i == n){ //n번째에서 단축될 시 음수 사이클 존재
-                        System.out.println(-1);
-                        return;
+        for(int i = 1; i <= n; i++){
+            for(Road road : roads){
+                if(dp[road.s] == Long.MAX_VALUE) continue;
+                if(dp[road.e] > dp[road.s] + road.t){
+                    if(i == n){
+                        System.out.println(-1); return;
                     }
-                    dp[r.e] = dp[r.s] + r.t;
-                }
 
+                    dp[road.e] = dp[road.s] + road.t;
+                }
             }
         }
 
-        for(int i = 1; i<= n; i++){
-            if(i == x) continue;
+        for(int i = 2; i <= n; i++) {
             if(dp[i] == Long.MAX_VALUE) System.out.println(-1);
             else System.out.println(dp[i]);
         }
-
     }
+
     static class Road{
-        int s;
-        int e;
-        int t;
+        int s,e,t;
     }
-
 }
