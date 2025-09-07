9b31060a3d6a119d857109c21e1c6473271e7e53
py0o0 <pantom0425@naver.com>
2025-09-07 20:28:04 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,37 +1,42 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        long[] v = new long[n]; 
-        for (int i = 0; i < n; i++) v[i] = Long.parseLong(st.nextToken());
+        int[] v = new int[n];
+        for(int i = 0; i < n; i++) v[i] = Integer.parseInt(st.nextToken());
 
         Arrays.sort(v);
 
         int an = 0;
-        for (int i = 0; i < n; i++) { 
+        for(int i = 0; i < n; i++){
             int start = 0;
             int end = n - 1;
 
-            while (start < end) {
-                if (start == i) { start++; continue; }
-                if (end == i) { end--; continue; }
-
-                long val = v[start] + v[end]; 
-                if (val == v[i]) {
-                    an++;
-                    break;
+            while(start < end){
+                if(i == start) {
+                    start++; continue;
                 }
-                if (val < v[i]) start++;
+                if(i == end) {
+                    end--; continue;
+                }
+
+                int val = v[start] + v[end];
+                if(val == v[i]) {
+                    an++; break;
+                }
+                if(val < v[i]) start++;
                 else end--;
             }
         }
         System.out.println(an);
     }
+
 }
