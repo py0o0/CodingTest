be63d92af487b6291284c3ba209c313745a9633d
py0o0 <pantom0425@naver.com>
2025-09-07 20:58:33 +0900

    [Gold III] Title: 불!, Time: 520 ms, Memory: 70412 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,103 +1,96 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-    class Main {
+class Main{
 
-        public static char[][] map;
-        public static int r,c;
-        public static int[][] visit;
-        public static int[] dx = {1,-1,0,0};
-        public static int[] dy = {0,0,-1,1};
-        public static Queue<xy> fire;
-        public static Queue<xy> people;
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+    public static Queue<xy> fire = new LinkedList<>();
+    public static Queue<xy> p =  new LinkedList<>();
+    public static char[][] map;
+    public static int[][] visit;
+    public static int[] dx = {1,-1,0,0};
+    public static int[] dy = {0,0,1,-1};
+    public static int n,m,cnt;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-            st = new StringTokenizer(br.readLine());
-            r = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            map = new char[r][c];
-            visit = new int[r][c];
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-
-            people = new LinkedList<>();
-            fire = new LinkedList<>();
-
-            for(int i = 0; i<r; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < c; j++) {
-                    map[i][j] = line.charAt(j);
-                    if(map[i][j] == 'J') {
-                        people.add(new xy(i, j));
-                        visit[i][j] = 1;
-                    }
-                    else if(map[i][j] == 'F')
-                        fire.add(new xy(i,j));
+        map = new char[n][m];
+        visit = new int[n][m];
+        for(int i = 0; i < n; i++){
+            String s = br.readLine();
+            for(int j = 0; j < m; j++){
+                map[i][j] = s.charAt(j);
+                if(map[i][j] == 'F'){
+                    fire.add(new xy(i,j));
+                }
+                else if(map[i][j] == 'J'){
+                    p.add(new xy(i,j));
+                    visit[i][j] = 1;
                 }
             }
-            int turn = 1;
-
-            while(true){
-                if(move())
-                    break;
-                fire();
-                if(people.size() == 0){
-                    System.out.print("IMPOSSIBLE");
-                    return;
-                }
-                turn++;
-            }
-            System.out.print(turn);
-
         }
-        static boolean move(){
-            int size = people.size();
-            while(size > 0) {
-                size--;
-                xy x = people.poll();
-                if (map[x.x][x.y] == 'F') continue;
-
-                for (int k = 0; k < 4; k++) {
-                    int nx = x.x + dx[k];
-                    int ny = x.y + dy[k];
-                    if (nx < 0 || nx >= r || ny < 0 || ny >= c)
-                        return true;
-
-                    if (map[nx][ny] == 'F' || map[nx][ny] == '#' || visit[nx][ny] == 1) continue;
-
-                    visit[nx][ny] = 1;
-                    people.add(new xy(nx, ny));
-                }
+        cnt = 1;
+        while(true){
+            if(move()){
+                System.out.println(cnt);
+                return;
             }
-            return false;
+            fire();
+            cnt++;
+
+            if(p.size() == 0) break;
         }
-
-        static void fire(){
-            int size = fire.size();
-            while(size > 0){
-                size--;
-                xy y = fire.poll();
-
-                for(int k = 0; k<4;k++){
-                    int nx = y.x + dx[k];
-                    int ny = y.y + dy[k];
-                    if(nx < 0 || nx >= r || ny <0 || ny >= c) continue;
-                    if(map[nx][ny]== 'F' || map[nx][ny]=='#') continue;
-                    map[nx][ny] = 'F';
-                    fire.add(new xy(nx, ny));
-                }
-
-            }
-        }
-
-        static class xy{
-            int x;
-            int y;
-            xy(int x,int y){
-                this.x = x;
-                this.y = y;
-            }
-        }
-
+        System.out.println("IMPOSSIBLE");
     }
+
+    public static boolean move(){
+        int size = p.size();
+        while(size-- > 0){
+            xy x = p.poll();
+            if(map[x.x][x.y] == 'F') continue;
+            for(int i = 0; i < 4; i++){
+
+                int nx = x.x + dx[i];
+                int ny = x.y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    return true;
+
+                if(visit[nx][ny] == 1 || map[nx][ny] == 'F'|| map[nx][ny] == '#') continue;
+
+                p.add(new xy(nx,ny));
+                visit[nx][ny] = 1;
+            }
+        }
+        return false;
+    }
+    public static void fire(){
+        int size = fire.size();
+        while(size-- > 0){
+            xy f = fire.poll();
+
+            for(int i = 0; i < 4; i++){
+                int nx = f.x + dx[i];
+                int ny = f.y + dy[i];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(map[nx][ny] == 'F' || map[nx][ny] == '#') continue;
+
+                map[nx][ny] = 'F';
+                fire.add(new xy(nx,ny));
+            }
+        }
+    }
+    public static class xy{
+        int x;
+        int y;
+        public xy(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
