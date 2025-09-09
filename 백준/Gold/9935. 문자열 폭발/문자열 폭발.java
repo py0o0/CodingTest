d08e6fc88a506540282d4ddf931e380fd90a881d
py0o0 <pantom0425@naver.com>
2025-09-09 23:32:42 +0900

    [Gold IV] Title: 문자열 폭발, Time: 496 ms, Memory: 62856 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,42 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-    class Main {
+public class Main{
 
-        public static void main(String[] args) throws IOException{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
 
-            String str1 = br.readLine();
-            String str2 = br.readLine();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-            Stack<Character> stk = new Stack<>();
+        String str = br.readLine();
+        String boom = br.readLine();
 
-            for(int i =0; i< str1.length(); i++){
-                stk.push(str1.charAt(i));
-
-                if(stk.size() >= str2.length() && stk.peek().equals(str2.charAt(str2.length() - 1))){
-                    StringBuilder str3 = new StringBuilder();
-                    for(int j = 0; j<str2.length(); j++){
-                        str3.append(stk.pop());
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            stk.push(str.charAt(i));
+            if(stk.size() >= boom.length() && stk.peek() == boom.charAt(boom.length()-1)){
+                int flag = 0;
+                for(int j = 1; j <= boom.length(); j++){
+                    if(stk.get(stk.size() - j) != boom.charAt(boom.length() - j)){
+                        flag = 1; break;
                     }
-                    str3 = str3.reverse();
-                    if(!str2.equals(str3.toString())){
-                        for(int j = 0; j<str3.length(); j++)
-                            stk.push(str3.charAt(j));
-                    }
-
                 }
+                if(flag == 0){
+                    for(int j = 0; j < boom.length(); j++) stk.pop();
+                }
+
             }
-            StringBuilder an = new StringBuilder();
-            while(!stk.isEmpty())
-                an.append(stk.pop());
-            an = an.reverse();
-            if(an.length() == 0)
-                System.out.println("FRULA");
-            else
-                System.out.println(an);
         }
-
-
+        StringBuilder an = new StringBuilder();
+        while(!stk.isEmpty()){
+            an.append(stk.pop());
+        }
+        if(an.toString().isEmpty()) System.out.println("FRULA");
+        else System.out.println(an.reverse());
     }
+}
