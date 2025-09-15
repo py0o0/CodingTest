e8eafa297b1c4c0f4b4c69769624e04291e68af9
py0o0 <pantom0425@naver.com>
2025-09-15 14:44:32 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 148 ms, Memory: 15616 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,41 +1,30 @@
-import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
 
-    static int an;
-    static int[][] dp = new int[10001][4];
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
 
+        int[][] dp = new int[10001][4];
         dp[1][1] = 1;
+        dp[2][1] = 1; dp[2][2] = 1;
+        dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
 
-        dp[2][1] = 1;
-        dp[2][2] = 1;
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
 
-        dp[3][1] = 1;
-        dp[3][2] = 1;
-        dp[3][3] = 1;
-        for(int i = 0; i < t; i++){
-            int n = sc.nextInt();
-            an = 0;
-            
-            if(dp[n][1] != 0){
-                System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
-                continue;
-            }
-
-            for(int j = 4; j <= n; j++){
-                dp[j][1] = dp[j-1][1];
-                dp[j][2] = dp[j-2][1] + dp[j-2][2]; // i-2는 중복제거를 위함. 마지막 2를 더할 땐 중복 제거를 위해 2를 1가지 방법으로 더함 +2 ,(1+1)은 중복 되는 방법이기에 X
-                dp[j][3] = dp[j-3][1] + dp[j-3][2] + dp[j-3][3];
+            for(int i = 4; i <= n; i++){
+                if(dp[i][1] == 1) continue;
+                dp[i][1] = 1;
+                dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+                dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
             }
 
             System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
         }
-
     }
 }
