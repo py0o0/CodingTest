efebab2ea2eaa9020be5cae39db12c6ae3d5914b
py0o0 <pantom0425@naver.com>
2025-09-16 14:42:53 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,7 +1,7 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main{
+public class Main{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,24 +10,17 @@ class Main{
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int target = Integer.parseInt(st.nextToken());
-        int[] dp = new int[target+1];
-
-        int[] coins = new int[n];
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            coins[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(coins);
+        int[] v = new int[n];
+        for (int i = 0; i < n; i++) v[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(v);
+        int[] dp = new int[target + 1];
         dp[0] = 1;
-
-        for(int coin : coins){
+        for(int coin : v){
             for(int j = coin; j <= target; j++){
-                dp[j] += dp[j - coin];
+                dp[j] = dp[j] + dp[j - coin];
             }
         }
         System.out.println(dp[target]);
 
     }
-
-
 }
