dfe769ee15c1688d1ecfa5246383be0ae363bac5
py0o0 <pantom0425@naver.com>
2025-09-19 17:02:57 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 456 ms, Memory: 37236 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,14 +1,14 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main{
+public class Main{
 
-    static int[][] map;
-    static int[][] dp;
     static int n;
-    static int[] dx = {0, 0, 1, -1};  // 4방향 이동
-    static int[] dy = {1, -1, 0, 0};
-    public static void main(String[] args) throws IOException{
+    static int[][] map;
+    static int [][] dp;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -16,47 +16,35 @@ class Main{
         map = new int[n][n];
         dp = new int[n][n];
 
-
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            for(int j = 0; j < n; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
-
 
         int max = 0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++){
-                max = Math.max(max, dfs(i,j));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j], dfs(i, j));
+                max = Math.max(max, dp[i][j]);
             }
-
+        }
         System.out.println(max);
-
     }
     static int dfs(int x, int y){
-        if (dp[x][y] != 0) return dp[x][y]; // 이미 방문한 곳이면 반환 (메모이제이션)
+        if(dp[x][y] != 0) return dp[x][y];
 
-        dp[x][y] = 1; // 자기 자신 포함
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        dp[x][y] = 1;
 
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] > map[x][y]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
-            }
+        for(int k = 0; k < 4; k++){
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+            if(map[x][y] >= map[nx][ny]) continue;
+
+            dp[x][y] = Math.max(1 + dfs(nx, ny), dp[x][y]);
         }
+
         return dp[x][y];
     }
-
-    static class xy{
-        int x,y,val;
-        xy(int x,int y,int val){
-            this.x = x;
-            this.y = y;
-            this.val = val;
-        }
-    }
-
-
 }
