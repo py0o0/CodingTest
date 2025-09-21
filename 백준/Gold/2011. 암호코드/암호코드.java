0779ef2e21b5c537784b31e54279792acfd53060
py0o0 <pantom0425@naver.com>
2025-09-21 11:38:03 +0900

    [Gold V] Title: 암호코드, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.java
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -1,39 +1,32 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
+public class Main {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+        String s = br.readLine();
+        long[]dp = new long[s.length() + 1];
 
-            String s = br.readLine();
-
-            int[] dp = new int[s.length() + 1];
-            dp[0] = 1;
-            for (int i = 1; i <= s.length(); i++) {
-                int cur = s.charAt(i - 1) - '0';
-                if(cur >= 1 && cur <= 9) 
-                    dp[i] += dp[i - 1];
-                    
-                
-
-                if(i == 1) continue;
-
-                int prev = s.charAt(i - 2) - '0';
-                prev = prev * 10 + cur;
-
-                if(prev >= 10 && prev <= 26) {
-                    dp[i] += dp[i - 2];
-                    dp[i] %= 1000000;
-                }
-            }
-            System.out.println(dp[s.length()]);
-
-
+        if (s.charAt(0) == '0') {
+            System.out.println(0);
+            return;
         }
 
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= s.length(); i++){
+            int cur = s.charAt(i - 1) - '0';
+            int prev = s.charAt(i - 2) - '0';
+            prev = prev * 10 + cur;
+
+            if(cur >= 1 && cur <= 9) dp[i] += dp[i - 1];
+            if(prev >= 10 && prev <= 26) dp[i] += dp[i - 2];
+
+            dp[i] %= 1000000;
+        }
+        System.out.println(dp[s.length()]);
     }
+}
