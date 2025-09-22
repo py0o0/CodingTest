7231408a5e2e7e8dcd8a1e0529647badcaffea7e
py0o0 <pantom0425@naver.com>
2025-09-22 14:26:09 +0900

    [Gold V] Title: 조 짜기, Time: 136 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.java
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -1,39 +1,37 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
+public class Main {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int n = Integer.parseInt(br.readLine());
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
-
-            int n = Integer.parseInt(br.readLine());
-            int[] v = new int[n + 1];
-            st = new StringTokenizer(br.readLine());
-            for(int i = 1; i <= n; i++)
-                v[i] = Integer.parseInt(st.nextToken());
-
-            int[] dp = new int[n + 1];
-
-            for(int i = 1; i<= n;i++){
-                int max = v[i]; int min = v[i];
-                for(int j = i-1; j > 0; j--){
-                    max = Math.max(max, v[j]);
-                    min = Math.min(min, v[j]);
-                    dp[i] = Math.max(dp[i], dp[j - 1] + max - min);
-                }
-            }
-            System.out.println(dp[n]);
-
-
-
-
+        if(n < 2){
+            System.out.println("0"); return;
         }
 
+        int[] v = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) v[i] = Integer.parseInt(st.nextToken());
 
+        int[] dp = new int[n];
 
+        dp[1] = Math.abs(v[0] - v[1]);
+
+        for(int i = 2; i < n; i++){
+            int max = v[i];
+            int min = v[i];
+
+            for(int j = i - 1; j > 0; j--){
+                max = Math.max(max, v[j]);
+                min = Math.min(min, v[j]);
+
+                dp[i] = Math.max(dp[i], dp[j - 1] + (max - min));
+            }
+        }
+        System.out.println(dp[n-1]);
     }
+}
