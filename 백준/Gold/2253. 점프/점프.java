939531c31c47eef7d09b9b6230b7eeedd8c2e204
py0o0 <pantom0425@naver.com>
2025-09-22 14:51:06 +0900

    [Gold IV] Title: 점프, Time: 208 ms, Memory: 57092 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.java
+++ 백준/Gold/2253. 점프/점프.java
@@ -1,70 +1,58 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
+public class Main {
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-            int[][] map = new int[n + 1][500];
-            int[] disconnect = new int[n + 1];
-            for (int i = 0; i <= n; i++)
-                Arrays.fill(map[i], Integer.MAX_VALUE);
-
-            for(int i = 0; i<m; i++) {
-                int x = Integer.parseInt(br.readLine());
-                disconnect[x] = -1;
+        int[][] dp = new int[10001][200];
+        for(int i =0; i < m; i++){
+            int x = Integer.parseInt(br.readLine());
+            dp[x][0] = -1;
+            if(x == 2){
+                System.out.println(-1);
+                return;
             }
+        }
 
-            int[] dj = {1,0,-1};
-            PriorityQueue<xy> pq = new PriorityQueue<>((a,b) -> b.x - a.x);
-            pq.add(new xy(1,0,0));
-            while(!pq.isEmpty()) {
-                xy x = pq.poll();
+        Queue<xy>q = new LinkedList<>();
+        q.add(new xy(2, 1, 1));
+        dp[1][0] = 1;
+        dp[2][1] = 1;
 
-                for(int i = 0; i<3; i++){
-                    int nj = (x.prev + dj[i]);
-                    int nx = x.x + nj;
-                    if(nj < 1 ||nx <1 || nx > n) continue;
-                    if(disconnect[nx] == -1) continue;
+        int[] dx = {-1,0,1};
+        while(!q.isEmpty()){
+            xy x = q.poll();
 
-                    if(map[nx][nj] <= x.cnt + 1) continue;
+            for(int i = 0; i < 3; i++){
+                int jump = x.jump + dx[i];
+                if(jump < 1) continue;
+                int nx = x.cur + jump;
 
-                    map[nx][nj] = x.cnt + 1;
-                    if (nx == n) continue;
-
-                    pq.add(new xy(nx,x.cnt + 1,nj));
-
+                if(nx == n){
+                    System.out.println(x.cnt + 1); return;
                 }
-
+                if(nx > n || dp[nx][0] == -1 || dp[nx][jump] != 0) continue;
+                dp[nx][jump] = x.cnt + 1;
+                q.add(new xy(nx, jump, dp[nx][jump]));
             }
-            int an = Integer.MAX_VALUE;
-            for(int i =0; i< 500;i++)
-                an = Math.min(an,map[n][i]);
-            if(an == Integer.MAX_VALUE) System.out.println(-1);
-            else System.out.println(an);
 
         }
-        static class xy{
-            int x;
-            int cnt;
-            int prev;
-            xy(int x,int cnt, int prev) {
-                this.x = x;
-                this.cnt = cnt;
-                this.prev = prev;
-            }
-        }
-
-
-
-
+        System.out.println(-1);
 
     }
+    static class xy{
+        int cur, jump, cnt;
+        xy(int cur, int jump, int cnt){
+            this.cur = cur;
+            this.jump = jump;
+            this.cnt = cnt;
+        }
+    }
+}
