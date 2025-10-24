bf9fc48525cba46c72c485e000f7cae6d6c5be61
py0o0 <pantom0425@naver.com>
2025-10-24 09:48:52 +0900

    [level 4] Title: 미로 탈출, Time: 196.71 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/4/81304. 미로 탈출/미로 탈출.java
+++ 프로그래머스/4/81304. 미로 탈출/미로 탈출.java
@@ -6,75 +6,77 @@ class Solution {
         ArrayList<Integer> trap = new ArrayList<>();
         
         for(int i = 0; i <= n; i++){
-            for(int j = 0; j < 2; j++) road[i][j] = new ArrayList<>();
+            road[i][0] = new ArrayList<>();
+            road[i][1] = new ArrayList<>();
         }
-        for(int[] r : roads){
-            road[r[0]][0].add(new Road(r[1], r[2])); //함정 밟지 않음
-            road[r[1]][1].add(new Road(r[0], r[2])); //함정 밟음
+        for(int r[] : roads){
+            road[r[0]][0].add(new Road(r[1],r[2]));
+            road[r[1]][1].add(new Road(r[0],r[2]));
         }
-        for(int t : traps){
+        
+        for(int t : traps)
             trap.add(t);
-        }
         
         int[][] dp = new int[n + 1][1 << trap.size()];
-        for (int i = 1; i <= n; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        for(int i = 0; i <= n; i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         
-        PriorityQueue<xy> pq = new PriorityQueue<>((a,b)->a.cost - b.cost);
+        PriorityQueue<xy> pq = new PriorityQueue<>((a,b)->a.dis-b.dis);
         pq.add(new xy(start, 0, 0));
         
         while(!pq.isEmpty()){
             xy x = pq.poll();
             
-            if(x.cur == end){
-                return x.cost;
+            if(x.x == end){
+                return x.dis;
             }
             
-            int trapState = x.state;
+            int trapState = x.visit;
+            int curTrap = 0;
+            
             for(int i = 0; i < trap.size(); i++){
-                if(trap.get(i) == x.cur){
-                    trapState ^= (1 << i); // 트랩 상태
+                if(trap.get(i) == x.x){
+                    trapState ^= (1 << i);
+                    curTrap = (trapState >> i) & 1;
                     break;
                 }
             }
             
-            
             for(int next = 1; next <= n; next++){
-                int dir = 0;
-                int curTrap = 0, nextTrap = 0;
-                
+                int nextTrap = 0;
                 for(int i = 0; i < trap.size(); i++){
-                    if(trap.get(i) == x.cur && ((trapState >> i) & 1) == 1)
-                        curTrap = 1;
-                    if(trap.get(i) == next && ((trapState >> i) & 1) == 1)
-                        nextTrap = 1;
-                }
-                
-                int index = curTrap ^ nextTrap;
-                
-                for(Road r : road[x.cur][index]){
-                    if(r.x != next) continue;
-                    if(dp[r.x][trapState] > x.cost + r.dis){
-                        dp[r.x][trapState] = x.cost + r.dis;
-                        pq.add(new xy(r.x,  x.cost + r.dis ,trapState));
+                    if(next == trap.get(i)){
+                        nextTrap = (trapState >> i) & 1;
+                        break;
                     }
                 }
+                
+                int curRoad = curTrap ^ nextTrap;
+                for(Road r : road[x.x][curRoad]){
+                    if(r.x != next) continue;
+                    if(dp[r.x][trapState] > x.dis + r.dis){
+                        dp[r.x][trapState] = x.dis + r.dis;
+                        pq.add(new xy(r.x, x.dis + r.dis, trapState));
+                    }
+                }
+                
             }
-            
         }
+        
         
         return 0;
     }
-    static class xy{
-        int cur, cost, state;
-        xy(int a, int b, int c){
-            cur = a; cost = b; state = c;
-        }
-    }
-    
     static class Road{
         int x, dis;
         Road(int a, int b){
             x = a; dis = b;
         }
+    }
+    static class xy{
+        int x, dis, visit;
+        xy(int a, int b, int c){
+            x = a; dis = b; visit = c;
+        }
+        
     }
 }
