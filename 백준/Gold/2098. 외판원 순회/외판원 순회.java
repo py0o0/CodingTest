ab1e400a9f14820e20a8b0ec2b67b92045ed58df
py0o0 <pantom0425@naver.com>
2025-10-26 07:07:28 +0900

    [Gold I] Title: 외판원 순회, Time: 184 ms, Memory: 19500 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -13,13 +13,10 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
         dp = new int[n][1 << n];
-
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             Arrays.fill(dp[i], -1);
-            for(int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            for (int j = 0; j < n; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
 
         int an = tsp(0, 1);
@@ -28,23 +25,20 @@ public class Main {
 
     static int tsp(int cur, int visit){
         if(visit == (1 << n) - 1){
-            if(map[cur][0] == 0)
-                return Integer.MAX_VALUE;
+            if(map[cur][0] == 0) return Integer.MAX_VALUE;
             return map[cur][0];
         }
 
         if(dp[cur][visit] != -1) return dp[cur][visit];
 
         int minCost = Integer.MAX_VALUE;
-        for(int next = 0; next < n; next++) {
+        for(int next = 0; next < n; next++){
             if((visit & (1 << next)) != 0 || map[cur][next] == 0) continue;
-
             int cost = tsp(next, visit | (1 << next));
             if(cost == Integer.MAX_VALUE) continue;
             minCost = Math.min(minCost, cost + map[cur][next]);
         }
         return dp[cur][visit] = minCost;
     }
-
 
 }
