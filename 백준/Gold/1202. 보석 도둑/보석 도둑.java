ba7f5c91671d98d07d11aec4de3f70932fc26f99
py0o0 <pantom0425@naver.com>
2025-11-15 20:13:55 +0900

    [Gold II] Title: 보석 도둑, Time: 1564 ms, Memory: 134716 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,78 +1,48 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+        Item[] items = new Item[n];
+        int[] bags = new int[m];
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());;
-
-            Item[] items = new Item[n];
-
-            for(int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                items[i] = new Item();
-                items[i].w = Integer.parseInt(st.nextToken());
-                items[i].val = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(items,(a, b) -> {
-                return a.w != b.w ? a.w - b.w : a.val - b.val;
-            });
-
-            PriorityQueue<Integer> back = new PriorityQueue<>();
-
-            for(int i = 0; i < k; i++){
-                int w = Integer.parseInt(br.readLine());
-                back.add(w);
-            }
-
-            PriorityQueue<Integer> Max = new PriorityQueue<>((a, b) -> b - a);
-            int w = back.poll();
-            long an = 0;
-            for(int i = 0; i < n; i++){
-                if(w < items[i].w) {
-                    if(!Max.isEmpty())
-                        an += Max.poll();
-                    while (!back.isEmpty()) {
-                        w = back.poll();
-                        if(w >= items[i].w)
-                            break;
-                        if(!Max.isEmpty())
-                            an += Max.poll();
-                    }
-                    if(w < items[i].w)
-                        break;
-                }
-                Max.add(items[i].val);
-
-            }
-            if(w >= items[n-1].w){
-                if(!Max.isEmpty())
-                    an += Max.poll();
-                while(!back.isEmpty()){
-                    w = back.poll();
-                    if(!Max.isEmpty())
-                        an += Max.poll();
-                }
-            }
-
-
-            System.out.println(an);
-
-
-        }
-        static class Item{
-            int w;
-            int val;
-
+            items[i] = new Item();
+            items[i].w = Integer.parseInt(st.nextToken());
+            items[i].val = Integer.parseInt(st.nextToken());
         }
 
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            bags[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(items, (a,b)->a.w-b.w);
+        Arrays.sort(bags);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        long sum = 0;
+        int index = 0;
+
+        for(int bag : bags) {
+            while(index < n && bag >= items[index].w){
+                pq.add(items[index++].val);
+            }
+
+            if(!pq.isEmpty()) sum += pq.poll();
+        }
+
+        System.out.println(sum);
 
     }
+    static class Item{
+        int w, val;
+    }
+
+
+}
