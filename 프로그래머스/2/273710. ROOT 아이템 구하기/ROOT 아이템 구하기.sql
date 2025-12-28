ea617ceadd07cb0e50fde9624699656e169f244d
py0o0 <pantom0425@naver.com>
2025-12-28 21:03:35 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,7 +1,8 @@
-select a.ITEM_ID, ITEM_NAME
-from ITEM_INFO as a left join 
-ITEM_TREE as b on a.ITEM_ID = b.ITEM_ID
-where PARENT_ITEM_ID is null
-order by 1
-
-#ROOT 아이템을 찾아 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME)을 출력하는 SQL문을 작성해 주세요. 이때, 결과는 아이템 ID를 기준으로 오름차순 정렬해 주세요.
+select ITEM_ID, ITEM_NAME
+from ITEM_INFO
+where ITEM_ID in (
+    select ITEM_ID
+    from ITEM_TREE
+    where PARENT_ITEM_ID is null
+)
+order by ITEM_ID
