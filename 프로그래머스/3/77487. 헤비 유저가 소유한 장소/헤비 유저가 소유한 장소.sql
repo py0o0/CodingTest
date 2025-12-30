1b7e4772ddb7d45caaa47afc841df362e72acc6b
py0o0 <pantom0425@naver.com>
2025-12-30 11:45:18 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+select ID,
+    NAME,
+    HOST_ID
+from PLACES
+where HOST_ID in
+    (
+        select HOST_ID
+        from PLACES
+        group by HOST_ID
+        having count(*) >= 2
+    )
+order by ID
