94d38724680fe0aefb7587dc494932895d9ab403
py0o0 <pantom0425@naver.com>
2025-12-30 12:15:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,8 @@
-SELECT a.ANIMAL_ID, a.NAME
-from ANIMAL_INS as a join
-ANIMAL_OUTS as b on a.ANIMAL_ID = b.ANIMAL_ID
-where b.DATETIME < a.DATETIME
+-- 코드를 입력하세요
+select a.ANIMAL_ID,
+    a.NAME
+from ANIMAL_INS a
+    join ANIMAL_OUTS b
+    on a.ANIMAL_ID = b.ANIMAL_ID
+where a.DATETIME > b.DATETIME
 order by a.DATETIME
-
-#관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
