1c3b80d800fc8cef7d579f90662027e5ea1548ae
py0o0 <pantom0425@naver.com>
2025-12-31 11:27:48 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,19 +1,20 @@
-select a.EMP_NO,EMP_NAME,
+-- 코드를 작성해주세요
+select a.EMP_NO,
+    a.EMP_NAME,
     case
-        when avg(score) >= 96 then 'S'
-        when avg(score) >= 90 then 'A'
-        when avg(score) >= 80 then 'B'
-        else 'C'
-        end
-    as GRADE,
+        when avg(SCORE) >= 96 then "S"
+        when avg(SCORE) >= 90 then "A"
+        when avg(SCORE) >= 80 then "B"
+        else "C"
+    end as GRADE,
     case
-        when avg(score) >= 96 then sal * 0.2
-        when avg(score) >= 90 then sal * 0.15
-        when avg(score) >= 80 then sal * 0.1
+        when avg(SCORE) >= 96 then SAL * 0.2
+        when avg(SCORE) >= 90 then SAL * 0.15
+        when avg(SCORE) >= 80 then SAL * 0.1
         else 0
-        end
-    as BONUS
-from HR_EMPLOYEES a right join
-HR_GRADE b on a.EMP_NO = b.EMP_NO
-group by 1
-order by 1
+    end as BONUS
+from HR_EMPLOYEES a
+    join HR_GRADE b
+    on a.EMP_NO = b.EMP_NO
+group by a.EMP_NO
+order by a.EMP_NO
