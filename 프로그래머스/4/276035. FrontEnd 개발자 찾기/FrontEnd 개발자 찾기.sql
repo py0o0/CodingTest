54c3c014668b811559036924f832801d81461af0
py0o0 <pantom0425@naver.com>
2025-12-31 11:41:54 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,7 +1,12 @@
-select distinct a.ID, EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS as a join SKILLCODES as b
-where (SKILL_CODE & CODE) and CATEGORY like 'Front end'
-order by 1
-#DEVELOPERS 테이블에서 Front End 스킬을 가진 개발자의 정보를 조회하려 합니다. 조건에 맞는 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
-
-#결과는 ID를 기준으로 오름차순 정렬해 주세요.
+-- 코드를 작성해주세요
+select ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+from DEVELOPERS
+where SKILL_CODE & (
+    select sum(CODE)
+    from SKILLCODES
+    where CATEGORY like "Front End"
+) > 0
+order by ID
