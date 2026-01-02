bab78513189ad27d98215c333f63f02d515f9f19
py0o0 <pantom0425@naver.com>
2026-01-02 12:32:32 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -2,11 +2,10 @@
 select ITEM_ID,
     ITEM_NAME,
     RARITY
-from ITEM_INFO a
-where ITEM_ID not in
-    (
-        select PARENT_ITEM_ID
-        from ITEM_TREE
-        where PARENT_ITEM_ID is not null
+from ITEM_INFO
+where ITEM_ID not in(
+    select PARENT_ITEM_ID
+    from ITEM_TREE
+    where PARENT_ITEM_ID is not null
     )
 order by ITEM_ID desc
