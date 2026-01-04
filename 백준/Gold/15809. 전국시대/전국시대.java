3e94e7cbf9c0151f2df10640701621e9e6115c23
py0o0 <pantom0425@naver.com>
2026-01-04 18:52:06 +0900

    [Gold III] Title: 전국시대, Time: 928 ms, Memory: 41156 KB -BaekjoonHub

--- 백준/Gold/15809. 전국시대/전국시대.java
+++ 백준/Gold/15809. 전국시대/전국시대.java
@@ -12,65 +12,59 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        parents = new int[n + 1];
-        for(int i = 1; i <= n; i++) parents[i] = i;
-
         int[] power = new int[n + 1];
+        parents = new int[n + 1];
         for(int i = 1; i <= n; i++){
             power[i] = Integer.parseInt(br.readLine());
+            parents[i] = i;
         }
 
-        for(int i = 0; i < m; i++){
+        while(m-- > 0){
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
+            b = find(b);
+            c = find(c);
             if(a == 1){
-                power[find(c)] += power[find(b)];
+                power[c] += power[b];
                 union(b, c);
             }
             else{
-                b = find(b);
-                c = find(c);
-
                 if(power[b] > power[c]){
                     power[b] -= power[c];
                     union(c, b);
                 }
-                else if(power[b] == power[c]){
-                    parents[b] = 0;
-                    parents[c] = 0;
-                }
-                else{
+                else if(power[b] < power[c]){
                     power[c] -= power[b];
                     union(b, c);
                 }
-
+                else{
+                    parents[b] = 0;
+                    parents[c] = 0;
+                }
             }
         }
 
+        int an = 0;
+        List<Integer> list = new ArrayList<>();
         int[] v = new int[n + 1];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
         for(int i = 1; i <= n; i++){
-            int root = find(i);
-            if(root == 0) continue;
-            if(v[root] == 1) continue;
-            v[root] = 1;
-            pq.add(power[root]);
+            int x = find(i);
+            if(x == 0 || v[x] == 1) continue;
+            v[x] = 1;
+            list.add(power[x]);
+            an++;
         }
-        System.out.println(pq.size());
-        while(!pq.isEmpty()){
-            System.out.print(pq.poll() + " ");
-        }
+        list.sort((a, b) -> a - b);
 
+        System.out.println(an);
+        for(int x : list) System.out.print(x + " ");
 
     }
-
     static int find(int x){
-        if(x == parents[x]) return x;
+        if(parents[x] == x) return x;
         return parents[x] = find(parents[x]);
     }
 
@@ -79,6 +73,7 @@ public class Main {
         b = find(b);
         if(a != b) parents[a] = b;
     }
+
 
 
 }
