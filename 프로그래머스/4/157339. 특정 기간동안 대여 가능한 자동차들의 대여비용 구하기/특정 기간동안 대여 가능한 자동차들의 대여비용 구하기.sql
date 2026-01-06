b721454d81d38ee0b69f800236892c64faba9dce
py0o0 <pantom0425@naver.com>
2026-01-06 13:24:33 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,18 +1,16 @@
 -- 코드를 입력하세요
 select CAR_ID,
     a.CAR_TYPE,
-    round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100), 0) as FEE
+    round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100 ), 0) as FEE
 from CAR_RENTAL_COMPANY_CAR a
     join CAR_RENTAL_COMPANY_DISCOUNT_PLAN b
     on a.CAR_TYPE = b.CAR_TYPE
-    and a.CAR_TYPE in ("세단", "SUV")
-    and b.DURATION_TYPE like "30일 이상"
+    and a.CAR_TYPE in ("SUV", "세단")
+    and b.DURATION_TYPE like "30일%"
 where CAR_ID not in (
-    select CAR_ID	
+    select CAR_ID
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    where END_DATE >= "2022-11-01" 
-        and START_DATE <= "2022-11-30" 
-    )
-    and round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100), 0) >= 500000 
-    and round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100), 0) < 2000000
-order by FEE desc, a.CAR_TYPE, CAR_ID desc
+    where START_DATE <= "2022-11-30" and END_DATE >= "2022-11-01"
+) and round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100 ), 0) >= 500000
+and round(DAILY_FEE * 30 * ((100 - DISCOUNT_RATE) / 100 ), 0) < 2000000
+order by 3 desc, 2, 1 desc
