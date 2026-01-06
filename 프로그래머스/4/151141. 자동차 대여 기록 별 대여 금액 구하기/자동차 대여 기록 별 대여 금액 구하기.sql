e871dcc52e741df15a90b34a7f7896f926187336
py0o0 <pantom0425@naver.com>
2026-01-06 13:36:32 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,28 +1,28 @@
 -- 코드를 입력하세요
 select HISTORY_ID,
     case
-        when datediff(END_DATE, START_DATE) + 1 < 7 
-            then round(DAILY_FEE * (datediff(END_DATE, START_DATE) + 1), 0)
-        when datediff(END_DATE, START_DATE) + 1 < 30 
+        when datediff(END_DATE, START_DATE) + 1 < 7
+            then DAILY_FEE * (datediff(END_DATE, START_DATE) + 1)
+        when datediff(END_DATE, START_DATE) + 1 < 30
             then round(DAILY_FEE * (datediff(END_DATE, START_DATE) + 1) * (
                 select (100 - DISCOUNT_RATE) / 100
                 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                where DURATION_TYPE like "7일%" and CAR_TYPE = "트럭"
-                ), 0)
+                where CAR_TYPE = "트럭" and DURATION_TYPE like "7일%"
+            ), 0)
         when datediff(END_DATE, START_DATE) + 1 < 90
             then round(DAILY_FEE * (datediff(END_DATE, START_DATE) + 1) * (
                 select (100 - DISCOUNT_RATE) / 100
                 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                where DURATION_TYPE like "30일%" and CAR_TYPE = "트럭"
-                ), 0)
+                where CAR_TYPE = "트럭" and DURATION_TYPE like "30일%"
+            ), 0)
         else round(DAILY_FEE * (datediff(END_DATE, START_DATE) + 1) * (
-            select (100 - DISCOUNT_RATE) / 100
-            from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-            where DURATION_TYPE like "90일%" and CAR_TYPE = "트럭"
+                select (100 - DISCOUNT_RATE) / 100
+                from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                where CAR_TYPE = "트럭" and DURATION_TYPE like "90일%"
             ), 0)
     end as FEE
 from CAR_RENTAL_COMPANY_CAR a
     join CAR_RENTAL_COMPANY_RENTAL_HISTORY b
     on a.CAR_ID = b.CAR_ID
-    and a.CAR_TYPE like "트럭"
-order by FEE desc, HISTORY_ID desc
+    and a.CAR_TYPE = "트럭"
+order by 2 desc, 1 desc
