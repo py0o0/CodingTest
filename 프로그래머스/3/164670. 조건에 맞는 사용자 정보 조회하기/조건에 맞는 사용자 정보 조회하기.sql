c401426e871296e2a897c1ee313f4e46db2cf493
py0o0 <pantom0425@naver.com>
2026-01-06 13:11:24 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 select USER_ID,
     NICKNAME,
-    concat(CITY, " ", STREET_ADDRESS1, " ", STREET_ADDRESS2) as 전체주소,
+    concat(CITY, " ", STREET_ADDRESS1, " ",	STREET_ADDRESS2) as 전체주소,
     concat(substr(TLNO, 1, 3), "-", substr(TLNO, 4, 4), "-", substr(TLNO, 8, 4)) as 전화번호
 from USED_GOODS_USER
 where USER_ID in (
@@ -9,5 +9,5 @@ where USER_ID in (
     from USED_GOODS_BOARD
     group by WRITER_ID
     having count(*) >= 3
-    )
-order by USER_ID desc
+)
+order by 1 desc
