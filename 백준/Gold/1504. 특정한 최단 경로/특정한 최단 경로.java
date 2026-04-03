6a71073c9b03146748b5489a5486a73b201dac04
py0o0 <pantom0425@naver.com>
2026-04-03 18:13:53 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 732 ms, Memory: 75120 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,97 +1,84 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.*;
+import java.util.*;
+import java.io.*;
 
-    public class Main {
+public class Main {
 
+    static ArrayList<Road>[] roads;
+    static long[][] dp;
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        static ArrayList<Road>[] roads;
-        static int[][] dist;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+        roads = new ArrayList[n+1];
+        dp = new long[n+1][n + 1];
 
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-
-            int INF = 200000001;
-
-            roads = new ArrayList[n + 1];
-            dist = new int[n + 1][n + 1];
-
-            for (int i = 0; i <= n; i++) {
-                Arrays.fill(dist[i], INF);
-                roads[i] = new ArrayList<>();
-            }
-
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                int t = Integer.parseInt(st.nextToken());
-
-                roads[s].add(new Road(e, t));
-                roads[e].add(new Road(s, t));
-            }
-
-            st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-
-            dijkstra(1);
-            dijkstra(v1);
-            dijkstra(v2);
-
-
-            int min = Math.min(dist[1][v1] + dist[v1][v2] + dist[v2][n], dist[1][v2] + dist[v2][v1] + dist[v1][n]);
-            if (min >= INF)
-                min = -1;
-            System.out.println(min);
+        for(int i = 0; i <= n; i++) {
+            roads[i] = new ArrayList<>();
+            for(int j = 0; j <= n; j++)
+                dp[i][j] = Long.MAX_VALUE;
 
         }
 
-        static void dijkstra(int s) {
-            dist[s][s] = 0;
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            long t = Long.parseLong(st.nextToken());
 
-            PriorityQueue<xy> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.t));
-            pq.add(new xy(s, 0));
+            roads[x].add(new Road(y, t));
+            roads[y].add(new Road(x, t));
+        }
 
-            while (!pq.isEmpty()) {
-                xy x = pq.poll();
-                for (Road road : roads[x.now]) {
-                    if (dist[s][road.next] > dist[s][x.now] + road.t) {
-                        dist[s][road.next] = dist[s][x.now] + road.t;
-                        pq.add(new xy(road.next, dist[s][road.next]));
-                    }
+
+
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
+
+        dijkstra(1);
+        dijkstra(v1);
+        dijkstra(v2);
+
+        if(dp[1][v1] == Long.MAX_VALUE || dp[v1][v2] == Long.MAX_VALUE || dp[v2][n] == Long.MAX_VALUE){
+            System.out.println(-1);
+            return;
+        }
+
+        long an = Math.min(dp[1][v1] + dp[v1][v2] + dp[v2][n], dp[1][v2] + dp[v2][v1] + dp[v1][n]);
+        System.out.println(an);
+    }
+
+    static void dijkstra(int i){
+        dp[i][i] = 0;
+        PriorityQueue<Road> pq = new PriorityQueue<>((a, b) -> Long.compare(a.t, b.t));
+        pq.add(new Road(i, 0));
+
+        while(!pq.isEmpty()){
+            Road x = pq.poll();
+            if(dp[i][x.x] < x.t) continue;
+
+            for(Road road : roads[x.x]){
+                if(dp[i][road.x] > dp[i][x.x] + road.t){
+                    dp[i][road.x] = dp[i][x.x] + road.t;
+                    pq.add(new Road(road.x, dp[i][road.x]));
                 }
             }
-
-
         }
-
-        static class xy {
-            int now;
-            int t;
-
-            xy(int now, int t) {
-                this.now = now;
-                this.t = t;
-            }
-        }
-
-        static class Road {
-            int next;
-            int t;
-
-            Road(int n, int t) {
-                this.next = n;
-                this.t = t;
-            }
-        }
-
-
     }
+
+    static class Road{
+        int x;
+        long t;
+        public Road(int x, long t) {
+            this.x = x;
+            this.t = t;
+        }
+    }
+
+
+}
